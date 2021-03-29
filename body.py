2ffc937abf75e4d9fbed744797c0ead2e70ff7e9
FedorkinOfficial <fedorkinofficial@gmail.com>
2021-03-29 15:32:25 +0300

    3/29/2021

--- body.py
+++ body.py
@@ -5,69 +5,54 @@ bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(commands=['math'])
 def welcome(message):
-    bot.send_message(message.chat.id, "https://meet.google.com/cnd-unhv-sxj".format(message.from_user, bot.get_me()),
+    bot.send_message(message.chat.id, "https://meet.google.com/ubv-xkca-poq".format(message.from_user, bot.get_me()),
     parse_mode='html')
-@bot.message_handler(commands=['phy'])
+@bot.message_handler(commands=['otk'])
 def welcome(message):
-    bot.send_message(message.chat.id, "https://meet.google.com/axp-fpuh-hya".format(message.from_user, bot.get_me()),
+    bot.send_message(message.chat.id, "https://meet.google.com/efi-oyqm-ioo".format(message.from_user, bot.get_me()),
     parse_mode='html')
-@bot.message_handler(commands=['phylb'])
+@bot.message_handler(commands=['tekpz'])
 def welcome(message):
-    bot.send_message(message.chat.id, "https://meet.google.com/qnn-qwvt-ufn".format(message.from_user, bot.get_me()),
+    bot.send_message(message.chat.id, "https://meet.google.com/eyu-cpkt-wfs".format(message.from_user, bot.get_me()),
     parse_mode='html')
-@bot.message_handler(commands=['pro'])
+@bot.message_handler(commands=['ufm'])
 def welcome(message):
-    bot.send_message(message.chat.id, "meet.google.com/wkv-uadx-sjx".format(message.from_user, bot.get_me()),
+    bot.send_message(message.chat.id, "https://meet.google.com/tzd-kqud-cyp".format(message.from_user, bot.get_me()),
     parse_mode='html')
-@bot.message_handler(commands=['op'])
+@bot.message_handler(commands=['ver'])
 def welcome(message):
-    bot.send_message(message.chat.id, "https://meet.google.com/vfv-dtcs-pov".format(message.from_user, bot.get_me()),
+    bot.send_message(message.chat.id, "https://meet.google.com/qna-sgdx-ngr".format(message.from_user, bot.get_me()),
     parse_mode='html')
-@bot.message_handler(commands=['vvsp'])
+@bot.message_handler(commands=['prolk'])
 def welcome(message):
-    bot.send_message(message.chat.id, "http://meet.google.com/fhy-ocgi-oms".format(message.from_user, bot.get_me()),
+    bot.send_message(message.chat.id, "https://meet.google.com/ipi-agnn-yew".format(message.from_user, bot.get_me()),
     parse_mode='html')
-@bot.message_handler(commands=['phylbch'])
+@bot.message_handler(commands=['propz'])
 def welcome(message):
-    bot.send_message(message.chat.id, "https://meet.google.com/fog-duvj-mjp".format(message.from_user, bot.get_me()),
+    bot.send_message(message.chat.id, "https://meet.google.com/yqe-khrq-kmr".format(message.from_user, bot.get_me()),
+    parse_mode='html')
+@bot.message_handler(commands=['bzd'])
+def welcome(message):
+    bot.send_message(message.chat.id, "https://meet.google.com/urd-qptz-tsc".format(message.from_user, bot.get_me()),
     parse_mode='html')
 
 
 @bot.message_handler(commands=['tagall'])
 def welcome(message):
-    bot.send_message(message.chat.id, "@VladFedorkin \n @parirambururi \n @electrovagina \n @hitecu \n @K_Tsukishima".format(message.from_user, bot.get_me()),
+    bot.send_message(message.chat.id, "@VladFedorkin \n @onethousxndminusseven \n @electrovagina \n @hitecu \n @K_Tsukishima".format(message.from_user, bot.get_me()),
     parse_mode='html')
-    bot.send_message(message.chat.id, "@pixelated_batya \n @violetviole \n @raritet04ka \n @WaitForMeForever \n @Ryd_14".format(message.from_user, bot.get_me()),
+    bot.send_message(message.chat.id, "@pixelated_batya \n @violetviole \n @raritet04ka \n @Malakhova_Anna \n @Ryd_14".format(message.from_user, bot.get_me()),
     parse_mode='html')
     bot.send_message(message.chat.id, "@Blackprincer \n @Kirill_Volobuev \n @Slava_Beton \n @sandorka \n @skribex1337".format(message.from_user, bot.get_me()),
     parse_mode='html')
     bot.send_message(message.chat.id, "@brake0force \n @w3nzr \n @bespontovbIypirozhok \n @Max_Reid \n @DanilKalmykov".format(message.from_user, bot.get_me()),
     parse_mode='html')
-    bot.send_message(message.chat.id, "@bohdanbelmaz \n @tttreep \n @errrni0 \n @s1ava0 \n @nwah_outlander".format(message.from_user, bot.get_me()),
+    bot.send_message(message.chat.id, "@bohdanbelmaz \n @tttreep \n @errrni0 \n @nwah_outlander".format(message.from_user, bot.get_me()),
     parse_mode='html')
     
-@bot.message_handler(content_types=['text'])
-def lalala(message):
-    if("ссылку" in message.text):
-        bot.send_message(message.chat.id, "Блять, а я тут для чого? Нужна ссылка - воспользуйся мной!\nЕсли ты не просил ссылку, то скажи это моему разрабу")
-    elif("ссылка" in message.text):
-        bot.send_message(message.chat.id, "Блять, а я тут для чого? Нужна ссылка - воспользуйся мной!\nЕсли ты не просил ссылку, то скажи это моему разрабу")
-    elif("Ссылку" in message.text):
-        bot.send_message(message.chat.id, "Блять, а я тут для чого? Нужна ссылка - воспользуйся мной!\nЕсли ты не просил ссылку, то скажи это моему разрабу")
-    elif("ссыль" in message.text):
-        bot.send_message(message.chat.id, "Блять, а я тут для чого? Нужна ссылка - воспользуйся мной!\nЕсли ты не просил ссылку, то скажи это моему разрабу")
-    elif("Ссыль" in message.text):
-        bot.send_message(message.chat.id, "Блять, а я тут для чого? Нужна ссылка - воспользуйся мной!\nЕсли ты не просил ссылку, то скажи это моему разрабу")
-    elif("ссылку?" in message.text):
-        bot.send_message(message.chat.id, "Блять, а я тут для чого? Нужна ссылка - воспользуйся мной!\nЕсли ты не просил ссылку, то скажи это моему разрабу")
-    elif("ссылка?" in message.text):
-        bot.send_message(message.chat.id, "Блять, а я тут для чого? Нужна ссылка - воспользуйся мной!\nЕсли ты не просил ссылку, то скажи это моему разрабу")
-    elif("Ссылку?" in message.text):
-        bot.send_message(message.chat.id, "Блять, а я тут для чого? Нужна ссылка - воспользуйся мной!\nЕсли ты не просил ссылку, то скажи это моему разрабу")
-    elif("ссыль?" in message.text):
-        bot.send_message(message.chat.id, "Блять, а я тут для чого? Нужна ссылка - воспользуйся мной!\nЕсли ты не просил ссылку, то скажи это моему разрабу")
-    elif("Ссыль?" in message.text):
-        bot.send_message(message.chat.id, "Блять, а я тут для чого? Нужна ссылка - воспользуйся мной!\nЕсли ты не просил ссылку, то скажи это моему разрабу")
-
+@bot.message_handler(commands=['thank'])
+def welcome(message):
+    bot.send_message(message.chat.id, "Спасибо @WaitForMeForever от всей группы".format(message.from_user, bot.get_me()),
+    parse_mode='html')
 
 bot.polling(none_stop=True)
